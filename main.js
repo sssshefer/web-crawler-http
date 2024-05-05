94ed21bc181c503c090bed7c6dac8cabcd17cae4
Shefer Arkadii <arkadiy.shefer@gmail.com>
2024-05-05 17:40:18 +0300

    crawl page is recursive

--- main.js
+++ main.js
@@ -1,1 +1,23 @@
-console.log('hi')
+const {crawlPage} = require('./crawl')
+
+async function main() {
+    if (process.argv.length < 3) {
+        console.log('No website provided')
+        process.exit(1)
+    }
+    if (process.argv.length > 3) {
+        console.log('Too many command line args')
+        process.exit(1)
+    }
+
+    const baseURL = process.argv[2]
+
+    console.log("Starting crawl of ", baseURL)
+    const pages = await crawlPage(baseURL, baseURL, {})
+
+    for(const page of Object.entries(pages)){
+        console.log(page)
+    }
+}
+
+main()

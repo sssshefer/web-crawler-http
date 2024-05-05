6991715b2976fc33a9ced0db2f1569360afb928d
Shefer Arkadii <arkadiy.shefer@gmail.com>
2024-05-05 17:17:34 +0300

    Single page crawling with error handling

--- crawl.js
+++ crawl.js
@@ -1,12 +1,65 @@
-function normalizeURL(urlString){
+const {JSDOM} = require('jsdom')
+
+async function crawlPage(currentURL) {
+    console.log(`actively crawling: ${currentURL}`)
+
+    try {
+        const resp = await fetch(currentURL)
+        if (resp.status > 399) {
+            console.log(`error in fetch with status code ${resp.status} on page ${currentURL}`)
+            return
+        }
+
+        const contentType = resp.headers.get("content-type")
+
+        if (!contentType.includes("text/html")) {
+            console.log(`non html response, content type: ${contentType} on page ${currentURL}`)
+            return
+
+        }
+        console.log(await resp.text())
+    } catch (err) {
+        console.log(`error in fetch ${err.message} on page ${currentURL}`)
+    }
+
+}
+
+function getURLsFromHTML(htmlBody, baseURL) {
+    const urls = []
+    const dom = new JSDOM(htmlBody)
+    const linkElements = dom.window.document.querySelectorAll('a')
+    for (const linkElement of linkElements) {
+        if (linkElement.href.slice(0, 1) === '/') {
+            try {
+                const urlObj = new URL(`${baseURL}${linkElement.href}`)
+                urls.push(urlObj.href)
+            } catch (err) {
+                console.log('Error with relative url: ', err.message)
+            }
+        } else {
+            try {
+                const urlObj = new URL(linkElement.href)
+                urls.push(urlObj.href)
+            } catch (err) {
+                console.log('Error with absolute url: ', err.message)
+            }
+        }
+    }
+
+    return urls
+}
+
+function normalizeURL(urlString) {
     const urlObj = new URL(urlString)
     const hostPath = `${urlObj.hostname}${urlObj.pathname}`
-    if( hostPath.length>0 && hostPath.slice(-1)==='/'){
-return hostPath.slice(0,-1)
+    if (hostPath.length > 0 && hostPath.slice(-1) === '/') {
+        return hostPath.slice(0, -1)
     }
     return hostPath
 }
 
 module.exports = {
-    normalizeURL
+    normalizeURL,
+    getURLsFromHTML,
+    crawlPage
 }
